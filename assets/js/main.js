cd3159ee130b94fdd93190eddb2f5d43827ca56c
rtof06 <ricardoofilho06@gmail.com>
2024-08-12 16:33:22 -0300

    resultados do imc corrigidos

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,20 +8,43 @@ function main() {
 }
 
 function calculaIMC() {
-   let peso = document.querySelector("#peso");
-   peso = Number(peso.value);
-   let altura = document.querySelector("#altura");
-   altura = Number(altura.value)/100;
-
-   let imc = peso/altura**altura;
-   imc = imc.toFixed(2);
-   console.log(imc);
-   console.log(isNaN(imc));
    const resultado = document.querySelector(".resultado-final");
    resultado.style.display = "block";
 
+   let peso = document.querySelector("#peso");
+   peso = Number(peso.value);
+   let altura = document.querySelector("#altura");
+   altura = Number(altura.value) / 100;
+
+   let imc = peso / altura ** 2;
+   imc = imc.toFixed(2);
+
+
    if (isNaN(imc) === false) {
-      resultado.innerHTML = `Seu imc é ${imc}`;
+      if (imc < 18.5) {
+         resultado.innerHTML += `Seu imc é ${imc}`;
+         abaixoDoPeso(resultado);
+      }
+      else if (imc >= 18.5 <= 24.9) {
+         resultado.innerHTML += `Seu imc é ${imc}`;
+         pesoNormal(resultado);
+      }
+      else if (imc >= 25 <= 29.9) {
+         resultado.innerHTML += `Seu imc é ${imc}`;
+         sobrepeso(resultado);
+      }
+      else if (imc >= 30 <= 34.9) {
+         resultado.innerHTML += `Seu imc é ${imc}`;
+         obesidadeGrauUm(resultado);
+      }
+      else if (imc >= 35 <= 39.9) {
+         resultado.innerHTML += `Seu imc é ${imc}`;
+         obesidadeGrauDois(resultado);
+      }
+      else if (imc >= 40) {
+         resultado.innerHTML += `Seu imc é ${imc}`;
+         obesidadeGrauTres(resultado);
+      }
    } else {
       naNResult(resultado);
    }
@@ -31,7 +54,42 @@ function naNResult(resultado) {
    resultado.innerHTML = `Coloque um resultado válido!`;
    resultado.style.backgroundColor = "#ff0000";
    resultado.style.color = "#fff";
-   resultado.style.color
+}
+
+function abaixoDoPeso(resultado) {
+   resultado.innerHTML += `<p>Você está abaixo do peso.</p>`;
+   resultado.style.backgroundColor = "#1f79f7";
+   resultado.style.color = "#fff";
+}
+
+function pesoNormal(resultado) {
+   resultado.innerHTML += `<p>Você está com o peso normal.</p>`;
+   resultado.style.backgroundColor = "#02c002";
+   resultado.style.color = "#fff";
+}
+
+function sobrepeso(resultado) {
+   resultado.innerHTML += `<p>Você está em sobrepeso.</p>`;
+   resultado.style.backgroundColor = "#ffa500";
+   resultado.style.color = "#fff";
+}
+
+function obesidadeGrauUm(resultado) {
+   resultado.innerHTML += `<p>Você está com obesidade de grau 1.</p>`;
+   resultado.style.backgroundColor = "#ff6600";
+   resultado.style.color = "#fff";
+}
+
+function obesidadeGrauDois(resultado) {
+   resultado.innerHTML += `<p>Você está com obesidade de grau 2.</p>`;
+   resultado.style.backgroundColor = "#ff5100";
+   resultado.style.color = "#fff";
+}
+
+function obesidadeGrauTres(resultado) {
+   resultado.innerHTML += `<p>Você está com obesidade de grau 3.</p>`;
+   resultado.style.backgroundColor = "#720d00";
+   resultado.style.color = "#fff";
 }
 
 main();
