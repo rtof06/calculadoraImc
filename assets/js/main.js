ddc06ace127ff09b751907c3bdfd3b29d18dd067
rtof06 <ricardoofilho06@gmail.com>
2024-08-12 17:07:58 -0300

    correção nas condicionais dos resultados

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,19 +25,19 @@ function calculaIMC() {
          resultado.innerHTML = `<p>Seu imc é ${imc}</p>`;
          abaixoDoPeso(resultado);
       }
-      else if (imc >= 18.5 <= 24.9) {
+      else if (imc >= 18.5 && imc <= 24.9) {
          resultado.innerHTML = `Seu imc é ${imc}</Sp>`;
          pesoNormal(resultado);
       }
-      else if (imc >= 25 <= 29.9) {
+      else if (imc >= 25 && imc <= 29.9) {
          resultado.innerHTML = `<p>Seu imc é ${imc}</p>`;
          sobrepeso(resultado);
       }
-      else if (imc >= 30 <= 34.9) {
+      else if (imc >= 30 && imc <= 34.9) {
          resultado.innerHTML = `<p>Seu imc é ${imc}</p>`;
          obesidadeGrauUm(resultado);
       }
-      else if (imc >= 35 <= 39.9) {
+      else if (imc >= 35 && imc <= 39.9) {
          resultado.innerHTML = `<p>Seu imc é ${imc}</p>`;
          obesidadeGrauDois(resultado);
       }
